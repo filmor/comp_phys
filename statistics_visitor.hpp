0f0b3a5f16bd7d89e63d933cef324c609b8c16a8
Benedikt Sauer <filmor@gmail.com>
2011-02-14 18:26:15 +0100

    Tabs in statistics_visitor angepasst.

--- statistics_visitor.hpp
+++ statistics_visitor.hpp
@@ -7,22 +7,22 @@
 
 namespace trivial
 {
-	template<class World>
-	class rog_visitor : public const_visitor<World>
-	{
-		void visit(World const& world)
-		{
-			for(unsigned n = 0; n < world.get_clusters().size(); ++n)
-			{
-				const typename World::cluster_type & c = world.get_clusters()[n];
+    template<class World>
+    class rog_visitor : public const_visitor<World>
+    {
+        void visit(World const& world)
+        {
+            for(unsigned n = 0; n < world.get_clusters().size(); ++n)
+            {
+                const typename World::cluster_type & c = world.get_clusters()[n];
 
-				typedef typename World::position_type::float_vector_type flt_vec_t;
+                typedef typename World::position_type::float_vector_type flt_vec_t;
                 flt_vec_t center;
 
-				for(unsigned m = 0; m < c.get_size(); ++m)
-					center += flt_vec_t(c.get_particles()[m].position);
+                for(unsigned m = 0; m < c.get_size(); ++m)
+                    center += flt_vec_t(c.get_particles()[m].position);
 
-				center /= c.get_size();
+                center /= c.get_size();
 
                 float rog = 0.f;
                 for(unsigned m = 0; m < c.get_size(); ++m)
@@ -30,10 +30,10 @@ namespace trivial
                 rog /= c.get_size();
                 rog = std::sqrt(rog);
 
-				print(c.get_size(), rog);
-			}
-		}
-	};
+                print(c.get_size(), rog);
+            }
+        }
+    };
 }
 
 #endif
